b3f0a6c4341b8f6c0eb5c818e83a2abb85c09da3
prabin.sc1 <prabin.sc1@gmail.com>
2023-09-14 11:33:07 +0545

    database configured

--- BloggingBackend/src/main/java/prabin/blogging/backend/BloggingBackendApplication.java
+++ BloggingBackend/src/main/java/prabin/blogging/backend/BloggingBackendApplication.java
@@ -8,7 +8,7 @@ public class BloggingBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BloggingBackendApplication.class, args);
-		System.out.println("prabin");
 	}
 
 }
+
